a6fbafc9a3814e51619df5d999ecde161b04441b
Shaunak KD <sagardeyyash19@gmail.com>
2023-05-31 17:13:28 +0600

    update the redux

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import TodoHead from './TodoHead';
 import TodoBody from './TodoBody';
 
-const Todo = () => {
+const Todo: React.FC = () => {
     return (
-        <div className="flex justify-center items-center h-72">
+        <div className="flex justify-center items-center h-96">
             <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
                 <TodoHead />
                 <TodoBody />
--- src/redux/todos/actionTypes.tsx
+++ src/redux/todos/actionTypes.tsx
@@ -1,1 +1,6 @@
-
+export const ADDED = 'todos/added';
+export const TOGGLED = 'todos/toggled';
+export const COLORCHANGER = 'todos/colorchanger';
+export const DELETED = 'todos/deleted';
+export const COMPLETEDALL = 'todos/completedall';
+export const CLEARALL = 'todos/clearall';
