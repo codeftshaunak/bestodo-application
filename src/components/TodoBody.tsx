1ca76cb1edb435064042a5390d1e6738c074dc35
Shaunak KD <sagardeyyash19@gmail.com>
2023-06-03 18:07:39 +0600

    add todo

--- src/components/TodoBody.tsx
+++ src/components/TodoBody.tsx
@@ -7,29 +7,33 @@ const TodoBody: React.FC = () => {
     const todos = useSelector((state: any) => state.todos);
     const filters = useSelector((state: any) => state.filters);
 
+    const filterByStatus = (todo: any) => {
+        const { status } = filters;
+        switch (status) {
+            case "Complete":
+                return todo.completed
+            case "Incomplete":
+                return !todo.completed
+            default:
+                return true;
+        }
+    }
+
+    const filterByColor = (todo: any) => {
+        const { colors } = filters;
+        if (colors.length > 0) {
+            return colors.includes(todo?.color)
+        } else {
+            return true;
+        }
+    }
+
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {
                 todos
-                    .filter((todo: any) => {
-                        const { status } = filters;
-                        switch (status) {
-                            case "Complete":
-                                return todo.completed
-                            case "Incomplete":
-                                return !todo.completed
-                            default:
-                                return true;
-                        }
-                    })
-                    .filter((todo: any) => {
-                        const { colors } = filters;
-                        if (colors.length > 0) {
-                            return colors.includes(todo?.color)
-                        } else {
-                            return true;
-                        }
-                    })
+                    .filter(filterByStatus)
+                    .filter(filterByColor)
                     .map((todo: any) => {
                         return <TodoItem todo={todo} key={todo.id} />
                     })
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -56,7 +56,7 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
                 )}
             </div>
 
-            <div className="select-none flex-1 line-through">
+            <div className={`select-none flex-1 ${completed && "line-through"}`}>
                 {text}
             </div>
 
